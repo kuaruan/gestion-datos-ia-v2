7146b7deff2863057916f36a0dfb81e1b55c28e5
kuaruan <kuaruan.projects@gmail.com>
2026-03-15 20:40:32 -0300

    Cambios para iniciar con render

--- app.py
+++ app.py
@@ -8,5 +8,9 @@ def hello():
     return {"message": "Hola Mundo desde Docker y Python!", "status": "OK"}
 
 if __name__ == '__main__':
+    # Render asigna un puerto dinámico mediante la variable de entorno PORT
+    # Si no existe (como en tu PC local), usará el 5000 por defecto
     port = int(os.environ.get('PORT', 5000))
+    
+    # host='0.0.0.0' es obligatorio para que el contenedor escuche peticiones externas
     app.run(host='0.0.0.0', port=port)
